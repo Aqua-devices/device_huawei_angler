c15bb9abae72abda3c10f49a18937fdeb562e4be
calebcabob <calphonic@gmail.com>
2017-12-05 17:18:24 -0500

    Go home Launcher3!

--- aosp_angler.mk
+++ aosp_angler.mk
@@ -35,7 +35,6 @@ $(call inherit-product, device/huawei/angler/device.mk)
 $(call inherit-product-if-exists, vendor/huawei/angler/device-vendor.mk)
 
 PRODUCT_PACKAGES += \
-    Launcher3 \
     WallpaperPicker
 
 PRODUCT_PACKAGES += \
